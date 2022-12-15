70dd7ca12bd4b90341daf17cd6a9296b4fbaacfa
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-12-15 09:33:09 +0100

    MADE CHANGES

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'neas_fashion',
+    title: 'Neas Fashion - Buy the best clothes, shoes, bags and perfume right from the comfort of your phone.',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
