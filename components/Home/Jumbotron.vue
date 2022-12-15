0ac2db189e4d35e76532733e0c152b112bd61d3d
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-12-15 12:30:20 +0100

    made changes

--- components/Home/Jumbotron.vue
+++ components/Home/Jumbotron.vue
@@ -1,6 +1,13 @@
 <template>
   <section class="hero">
-    <img :src="bannerImage" alt="Neas Fashion Banner AD" id="banner_image">
+    <div class="desktop">
+      <div class="banner_ad">
+        <img :src="bannerImage" alt="Neas Fashion Banner AD" id="banner_ad_image">
+      </div>
+      <div class="other_ads">
+
+      </div>
+    </div>
   </section>
 </template>
 
@@ -8,7 +15,7 @@
 export default {
   data() {
     return {
-      bannerImage: 'https://firebasestorage.googleapis.com/v0/b/neas-fashion.appspot.com/o/Neas%20fashion%20banner%20ad.png?alt=media&token=5374a527-0324-4d5b-adf7-3cdf14ae83a6'
+      bannerImage: 'https://firebasestorage.googleapis.com/v0/b/neas-fashion.appspot.com/o/Banner%2FNeas%20fashion%20banner%20ad.png?alt=media&token=2938c29e-c3ff-4283-a578-638c9a75bd02'
     }
   }
 }
@@ -18,14 +25,19 @@ export default {
 .hero {
   margin: 2rem 3rem;
 
-  display: flex;
-  justify-content: flex-start;
+  .desktop {
+    display: flex;
+    justify-content: space-between;
 
-  #banner_image {
-    max-width: 90%;
-    height: 450px;
-    object-fit: contain;
-    border-radius: $standard-r;
+    .banner_ad {
+
+      #banner_ad_image {
+        max-width: 90%;
+        max-height: 450px;
+        object-fit: contain;
+        border-radius: $standard-r;
+      }
+    }
   }
 }
 </style>
