01ccee3fb96624d9ca42fe226fff2a8dfd88303f
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-12-15 16:33:45 +0100

    made changes

--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,19 @@
   <div class="home">
     <Jumbotron />
     <Categories />
+    <Recently />
   </div>
 </template>
 
 <script>
 import Jumbotron from "../components/Home/Jumbotron.vue"
 import Categories from "../components/Home/Categories.vue"
+import Recently from "../components/Home/Recently.vue"
 export default {
   components: {
     Jumbotron,
-    Categories
+    Categories,
+    Recently
   }
 }
 </script>
