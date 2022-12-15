f435b756e287a4aaee81db35301fb4d9f9108519
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-12-15 13:59:36 +0100

    made changes

---
+++ components/Home/Categories.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="categories">
+    <Headers>
+      <template #title>Trending Categories</template>
+    </Headers>
+  </div>
+</template>
+
+<script>
+import Headers from "../Global/Headers.vue"
+export default {
+  components: {
+    Headers
+  }
+}
+</script>
+
+<style>
+
+</style>
