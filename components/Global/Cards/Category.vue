9721ee87238001c03e613d001993cc93219e3706
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-12-15 14:20:58 +0100

    made changes

--- components/Global/Cards/Category.vue
+++ components/Global/Cards/Category.vue
@@ -1,10 +1,17 @@
 <template>
-  
+  <div class="category">
+    <div class="s">
+      <img loading="lazy" :src="item.image" :alt="'Neas Fashion Product Category - '+item.name">
+      <div class="_details">
+        <NuxtLink :to="'#/'+item.id">{{item.name}}</NuxtLink>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  props: ["item"]
 }
 </script>
 
