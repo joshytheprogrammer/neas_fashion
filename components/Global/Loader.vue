b735464111400569637e90f6afac822d1123e986
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-12-15 10:13:19 +0100

    made changes

---
+++ components/Global/Loader.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <grid-loader v-if="type == 'grid'" :color="styles.color" :size="styles.size"></grid-loader>
+    <clip-loader v-if="type == 'clip'" :color="styles.color" :size="styles.size"></clip-loader>
+    <bounce-loader v-if="type == 'bounce'" :color="styles.color" :size="styles.size"></bounce-loader>
+    <scale-loader v-if="type == 'scale'" :color="styles.color" :size="styles.size"></scale-loader>
+  </div>
+</template>
+
+<script>
+// docs @ https://greyby.github.io/vue-spinner/
+import GridLoader from 'vue-spinner/src/GridLoader.vue'
+import ClipLoader from 'vue-spinner/src/ClipLoader.vue'
+import BounceLoader from 'vue-spinner/src/BounceLoader.vue'
+import ScaleLoader from 'vue-spinner/src/ScaleLoader.vue'
+
+export default {
+  components: {
+    GridLoader,
+    ClipLoader,
+    BounceLoader,
+    ScaleLoader,
+  },
+  props: ['type'],
+  data() {
+    return {
+      styles: {
+        color: 'hsl(351, 81%, 27%)',
+        size: '64px'
+      }
+    }
+  }
+}
+</script>
