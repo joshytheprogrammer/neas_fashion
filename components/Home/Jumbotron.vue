bafa6ef8293ff0d5408ff8c468ba7a56fe8a3f24
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-12-17 10:44:13 +0100

    made changes

--- components/Home/Jumbotron.vue
+++ components/Home/Jumbotron.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="hero">
-    <img :src="bannerImage" alt="Neas Fashion Banner AD" id="banner_ad_image">
+    <img loading="lazy" :src="bannerImage" alt="Neas Fashion Banner AD" id="banner_ad_image">
   </section>
 </template>
 
