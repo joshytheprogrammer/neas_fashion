3e96bd73ae370b35a786dfe75d4e7089452f6d02
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-12-23 18:03:00 +0100

    made changes

--- components/App/List.vue
+++ components/App/List.vue
@@ -1,13 +1,41 @@
 <template>
-
+  <div class="list">
+    <section v-for="item in categories" :key="item.id">
+      <Headers>
+        <template #title> Some {{item.name}} </template>
+      </Headers>
+      {{item.name}}
+    </section>
+  </div>
 </template>
 
 <script>
+import Headers from "../Global/Headers.vue"
 export default {
-
+  components: {
+    Headers
+  },
+  data() {
+    return {
+      categories: []
+    }
+  },
+  async fetch() {
+    try {
+      await this.$fire.firestore.collection('categories').get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          this.categories.push({id: doc.id, name: doc.data().name})
+        })
+      })
+    } catch (error) {
+      this.error = error.message
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.list {
+  display: block;
+}
 </style>
